6c7e10077027f8d14a323a59e945f35e836d4542
korathbasil <korathbasil@live.com>
2020-09-29 20:14:02 +0530

    working on image uploads to GCP, installed and configured @google-cloud/Storage pkg

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+const http = require("http");
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const socketIo = require("socket.io");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const { Storage } = require("@google-cloud/storage");
 
 // Route imports
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
 const userRoute = require("./routes/user");
+const { pathToFileURL } = require("url");
 
 // App config
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 PORT = process.env.PORT || 8000;
 dotenv.config();
+const gc = new Storage({
+  keyFilename: path.join(__dirname, "facebook-clone-291012-cc3214523360.json"),
+  projectId: "facebook-clone-291012",
+});
+
+gc.getBuckets().then((buckets) => console.log(buckets));
+const imageBucket = gc.bucket("fb-clone-images");
 
 // DB config
 mongoose.connect(
@@ -23,6 +37,11 @@ mongoose.connect(
   },
   () => console.log("Database connected")
 );
+
+// Socket listener
+io.on("connection", (socket) => {
+  console.log(" user connected");
+});
 
 // Middlewares
 app.use(express.json());
@@ -44,4 +63,4 @@ app.post("/post", postRoute);
 app.put("/user", userRoute);
 
 // Server listener
-app.listen(PORT, () => console.log("server started at " + PORT));
+server.listen(PORT, () => console.log("server started at " + PORT));
