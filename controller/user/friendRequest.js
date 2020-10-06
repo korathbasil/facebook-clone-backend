3a768e333b110ee6187214847d7b39ddb0ef3273
korathbasil <korathbasil@live.com>
2020-10-06 22:59:15 +0530

    removed some junk code from user.js route

---
+++ controller/user/friendRequest.js
@@ -0,0 +1,104 @@
+// Model imports
+
+module.exports = {
+  sendRequest: (req, res) => {
+    const request = {
+      senderId: req.body.userId,
+      senderMiniId: req.body.miniUserId,
+      recieverId: req.body.targetUserId,
+      recieverMiniId: req.body.targetUserMiniId,
+    };
+    // Adding request to FriendRequest collection
+    FriendRequests.create(request, (err, data) => {
+      if (err) {
+        res.status(403).json({ message: "failed to send request" });
+      } else {
+        // Adding to friend request sent list of sending user
+        Users.findById(req.body.userId)
+          .then((user) => {
+            user.friendRequestsSent.push({
+              requestId: data._id,
+            });
+            return user.save();
+          })
+          .then(async (result) => {
+            const targetUserData = await Users.findById(req.body.targetUserId);
+            return {
+              targetUser: targetUserData,
+              result: result,
+            };
+          })
+          .then(async ({ targetUser, result }) => {
+            // console.log(targetUser);
+            targetUser.friendRequestsRecieved.push({
+              requestId: data._id,
+            });
+            savedTargetUser = await targetUser.save();
+            return { savedTargetUser: savedTargetUser, result: result };
+          })
+          .then(({ savedTargetUser, result }) => {
+            if (action) {
+              res.status(201).send("Friend Request sent successfully");
+            } else {
+              res.status(201).send("friend request deleted successfully");
+            }
+          })
+          .catch((e) => res.status(400).json({ m: e }));
+      }
+    });
+  },
+  acceptRequest: (req, res) => {
+    const requestId = req.body.requestId;
+    const action = req.body.action;
+    let senderId, senderMiniId, recieverId, recieverMiniId;
+    FriendRequests.findById(requestId)
+      .then(async (request) => {
+        senderId = request.senderId;
+        recieverId = request.recieverId;
+        senderMiniId = request.senderMiniId;
+        recieverMiniId = request.recieverMiniId;
+        // console.log(senderId);
+        // console.log(recieverId);
+        return request.remove();
+      })
+      .then((result) => {
+        Users.findById(senderId)
+          .then(async (user) => {
+            user.friendRequestsSent.splice(
+              user.friendRequestsSent.findIndex(
+                (rqst) => rqst.requestId === requestId
+              )
+            );
+            if (action) {
+              user.friends.push({
+                id: recieverId,
+                miniUserId: recieverMiniId,
+              });
+            }
+            return user.save();
+          })
+          .then((result) => console.log(result))
+          .catch((e) => console.log(e));
+        Users.findById(recieverId)
+          .then(async (user) => {
+            user.friendRequestsRecieved.splice(
+              user.friendRequestsSent.findIndex(
+                (rqst) => rqst.requestId === requestId
+              )
+            );
+            if (action) {
+              user.friends.push({
+                id: senderId,
+                miniUserId: senderMiniId,
+              });
+            }
+            return user.save();
+          })
+          .then((result) => console.log(result))
+          .catch((e) => console.log(e));
+        return result;
+      })
+      .then((result) => res.status(201).send("Accepted friendRequest"))
+      .catch((e) => res.status(403).send(e));
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,10 @@ const Photos = require("../model/Photo");
 const Posts = require("../model/Posts");
 
 // Controller imports
+const {
+  sendRequest,
+  acceptRequest,
+} = require("../controller/user/friendRequest");
 const { uploadPost } = require("../controller/post");
 
 const router = express.Router();
@@ -40,135 +44,10 @@ router.post("/getDetails", (req, res) => {
 });
 
 // Sending a friend request
-router.put("/friendRequest", (req, res) => {
-  const request = {
-    senderId: req.body.userId,
-    senderMiniId: req.body.miniUserId,
-    recieverId: req.body.targetUserId,
-    recieverMiniId: req.body.targetUserMiniId,
-  };
-  // Adding request to FriendRequest collection
-  FriendRequests.create(request, (err, data) => {
-    if (err) {
-      res.status(403).json({ message: "failed to send request" });
-    } else {
-      // Adding to friend request sent list of sending user
-      Users.findById(req.body.userId)
-        .then((user) => {
-          user.friendRequestsSent.push({
-            requestId: data._id,
-          });
-          return user.save();
-        })
-        .then(async (result) => {
-          const targetUserData = await Users.findById(req.body.targetUserId);
-          return {
-            targetUser: targetUserData,
-            result: result,
-          };
-        })
-        .then(async ({ targetUser, result }) => {
-          // console.log(targetUser);
-          targetUser.friendRequestsRecieved.push({
-            requestId: data._id,
-          });
-          savedTargetUser = await targetUser.save();
-          return { savedTargetUser: savedTargetUser, result: result };
-        })
-        .then(({ savedTargetUser, result }) => {
-          if (action) {
-            res.status(201).send("Friend Request sent successfully");
-          } else {
-            res.status(201).send("friend request deleted successfully");
-          }
-        })
-        .catch((e) => res.status(400).json({ m: e }));
-    }
-  });
-});
+router.put("/friendRequest", sendRequest);
 
 // Accepting or rejecting a friend request
-router.post("/friendRequest", (req, res) => {
-  const requestId = req.body.requestId;
-  const action = req.body.action;
-  let senderId, senderMiniId, recieverId, recieverMiniId;
-  FriendRequests.findById(requestId)
-    .then(async (request) => {
-      senderId = request.senderId;
-      recieverId = request.recieverId;
-      senderMiniId = request.senderMiniId;
-      recieverMiniId = request.recieverMiniId;
-      // console.log(senderId);
-      // console.log(recieverId);
-      return request.remove();
-    })
-    .then((result) => {
-      Users.findById(senderId)
-        .then(async (user) => {
-          user.friendRequestsSent.splice(
-            user.friendRequestsSent.findIndex(
-              (rqst) => rqst.requestId === requestId
-            )
-          );
-          if (action) {
-            user.friends.push({
-              id: recieverId,
-              miniUserId: recieverMiniId,
-            });
-          }
-          return user.save();
-        })
-        .then((result) => console.log(result))
-        .catch((e) => console.log(e));
-      Users.findById(recieverId)
-        .then(async (user) => {
-          user.friendRequestsRecieved.splice(
-            user.friendRequestsSent.findIndex(
-              (rqst) => rqst.requestId === requestId
-            )
-          );
-          if (action) {
-            user.friends.push({
-              id: senderId,
-              miniUserId: senderMiniId,
-            });
-          }
-          return user.save();
-        })
-        .then((result) => console.log(result))
-        .catch((e) => console.log(e));
-      return result;
-    })
-    .then((result) => res.status(201).send("Accepted friendRequest"))
-    .catch((e) => res.status(403).send(e));
-});
-
-// router.post("/profilePicture", (req, res) => {
-//   const file = req.files.image;
-//   const userId = req.body.userId;
-//   const fileName = new Date().toISOString();
-//   const fileExtension = file.name.split(".")[file.name.split(".").length - 1];
-//   streamifier.createReadStream(new Buffer(file.data)).pipe(
-//     imageBucket.file(`profilePictures/${file.name}`).createWriteStream({
-//       resumable: false,
-//       gzip: true,
-//     })
-//   );
-//   // const newPhoto = {
-//   //   userId: ,
-//   //   miniUserId: ,
-//   //   imageUrl: ,
-//   //   albumId: ,
-//   //   postId: ,
-//   // }
-//   //   Photos.create()
-//     const newPost = {
-//       variant =
-//     }
-//   Users.findById(userId).then((user) => {
-//     user.profilePicture = `https://storage.googleapis.com/fb-clone-images/profilePictures/${fileName}.${fileExtension}`;
-//   });
-// });
+router.post("/friendRequest", acceptRequest);
 
 router.post("/profilePicture", uploadPost);
 
