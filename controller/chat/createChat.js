3dadea569400611f25b0a03c4188d5f76c576c8b
korathbasil <korathbasil@live.com>
2020-10-15 23:37:23 +0530

    added some comments in creatchat

--- controller/chat/createChat.js
+++ controller/chat/createChat.js
@@ -2,28 +2,30 @@
 const Chat = require("../../model/Chat");
 const User = require("../../model/User");
 const MiniUser = require("../../model/MiniUser");
-const Message = require("../../model/Message");
 
 module.exports = (req, res) => {
   const newChat = {
     parties: [req.body.miniUserId, req.body.miniRecieverId],
     messages: [],
   };
+  // Creating new chat
   Chat.create(newChat)
     .then((chat) => {
+      // Adding id of created chat to Sender's chats array
       MiniUser.findById(req.body.miniUserId).then((miniUser) => {
         User.findById(miniUser.userId).then((user) => {
           user.chats.push(chat._id);
           user.save();
         });
       });
+      // Adding id of created chat to Reciever's chats array
       MiniUser.findById(req.body.miniRecieverId).then((miniUser) => {
         User.findById(miniUser.userId).then((user) => {
           user.chats.push(chat._id);
           user.save();
         });
       });
-      res.send("chat created");
+      res.status(201).send("chat created");
     })
     .catch((e) => res.send(e));
 };
