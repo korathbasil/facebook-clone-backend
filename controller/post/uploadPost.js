044b122f5c24111d4b0cb4e0f95908e1803c34ee
korathbasil <korathbasil@live.com>
2020-10-21 23:12:04 +0530

    upload post edited with support for posts without images

--- controller/post/uploadPost.js
+++ controller/post/uploadPost.js
@@ -2,6 +2,9 @@ const { Buffer } = require("buffer");
 const streamifier = require("streamifier");
 const imageBucket = require("../../util/GCPbucket");
 
+const { getIo } = require("../../socket-io");
+const io = getIo();
+
 // Model imports
 const Users = require("../../model/User");
 const Photos = require("../../model/Photo");
@@ -9,64 +12,103 @@ const Albums = require("../../model/Album");
 const Posts = require("../../model/Post");
 
 module.exports = (req, res) => {
-  let albumId, newImageId;
-  Albums.findOne({
-    userId: req.body.userId,
-    albumName: "Timeline Photos",
-  }).then((album) => {
-    albumId = album._id;
-  });
-  const newImage = {
-    userId: req.body.userId,
-    miniUserId: req.body.miniUserId,
-    small: req.images.small,
-    medium: req.images.medium,
-    original: req.images.original,
-    albumId: albumId,
-  };
-  Photos.create(newImage, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      newImageId = data._id;
-      Albums.findById(albumId).then((album) => {
-        album.latestPhoto = data._id;
-        album.photos.unshift(data._id);
-        album.save();
-      });
-      const post = {
-        variant: req.body.folder,
-        authorId: req.body.userId,
-        miniAuthorId: req.body.miniUserId,
-        caption: req.body.caption,
-        album: albumId,
-        image: data._id,
-      };
-      Posts.create(post, (err, data) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          Users.findById(data.authorId)
-            .then((user) => {
-              user.posts.unshift(data._id);
-              user.feed.unshift(data._id);
-              user.recentNinePhotos.unshift(newImageId);
-              user.friends.forEach((friend) => {
-                Users.findById(friend.id)
-                  .then((selectedFriend) => {
-                    selectedFriend.feed.unshift(data._id);
-                    selectedFriend.save();
-                  })
-                  .catch((e) => console.log(e));
-              });
-              user.save();
-            })
-            .then(() => {
-              res.status(201).send("done success");
-            })
-            .catch((e) => res.status(401).send(e));
-        }
-      });
-    }
-  });
+  if (req.hasImage) {
+    let albumId, newImageId;
+    Albums.findOne({
+      userId: req.body.userId,
+      albumName: "Timeline Photos",
+    }).then((album) => {
+      albumId = album._id;
+    });
+    const newImage = {
+      userId: req.body.userId,
+      miniUserId: req.body.miniUserId,
+      small: req.images.small,
+      medium: req.images.medium,
+      original: req.images.original,
+      albumId: albumId,
+    };
+    Photos.create(newImage, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        newImageId = data._id;
+        Albums.findById(albumId).then((album) => {
+          album.latestPhoto = data._id;
+          album.photos.unshift(data._id);
+          album.save();
+        });
+        const post = {
+          variant: req.body.folder,
+          authorId: req.body.userId,
+          miniAuthorId: req.body.miniUserId,
+          caption: req.body.caption,
+          album: albumId,
+          image: data._id,
+        };
+        Posts.create(post, (err, data) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            Users.findById(data.authorId)
+              .then((user) => {
+                user.posts.unshift(data._id);
+                user.feed.unshift(data._id);
+                user.recentNinePhotos.unshift(newImageId);
+                user.friends.forEach((friend) => {
+                  Users.findById(friend.id)
+                    .then((selectedFriend) => {
+                      selectedFriend.feed.unshift(data._id);
+                      selectedFriend.save();
+                    })
+                    .catch((e) => console.log(e));
+                });
+                user.save();
+              })
+              .then(() => {
+                io.to(toString(req.body.userId)).emit("new-post", {
+                  post: data,
+                });
+                res.status(201).send("done success");
+              })
+              .catch((e) => res.status(401).send(e));
+          }
+        });
+      }
+    });
+  } else {
+    const post = {
+      variant: req.body.folder,
+      authorId: req.body.userId,
+      miniAuthorId: req.body.miniUserId,
+      caption: req.body.caption,
+    };
+    Posts.create(post, (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        Users.findById(data.authorId)
+          .then((user) => {
+            user.posts.unshift(data._id);
+            user.feed.unshift(data._id);
+            user.friends.forEach((friend) => {
+              Users.findById(friend.id)
+                .then((selectedFriend) => {
+                  selectedFriend.feed.unshift(data._id);
+                  selectedFriend.save();
+                })
+                .catch((e) => console.log(e));
+            });
+            user.save();
+          })
+          .then(() => {
+            io.to(toString(req.body.userId)).emit("new-post", {
+              post: data,
+            });
+            res.status(201).send("done success");
+          })
+          .catch((e) => res.status(401).send(e));
+      }
+    });
+  }
 };
--- middlewares/addToBucket.js
+++ middlewares/addToBucket.js
@@ -5,49 +5,55 @@ const sharp = require("sharp");
 
 module.exports = async (req, res, next) => {
   const folder = req.body.folder;
-  const file = req.files.image;
-  const fileExtension = file.name.split(".")[file.name.split(".").length - 1];
-  const fileName = new Date().toISOString();
-  await streamifier.createReadStream(new Buffer(file.data)).pipe(
-    imageBucket
-      .file(`${folder}/original/${fileName}.${fileExtension}`)
-      .createWriteStream({
-        resumable: false,
-        gzip: true,
+  if (req.files) {
+    const file = req.files.image;
+    const fileExtension = file.name.split(".")[file.name.split(".").length - 1];
+    const fileName = new Date().toISOString();
+    await streamifier.createReadStream(new Buffer(file.data)).pipe(
+      imageBucket
+        .file(`${folder}/original/${fileName}.${fileExtension}`)
+        .createWriteStream({
+          resumable: false,
+          gzip: true,
+        })
+    );
+    await sharp(new Buffer(file.data))
+      .resize({ width: 360 })
+      .toBuffer()
+      .then((data) => {
+        streamifier.createReadStream(data).pipe(
+          imageBucket
+            .file(`${folder}/small/${fileName}.${fileExtension}`)
+            .createWriteStream({
+              resumable: false,
+              gzip: true,
+            })
+        );
       })
-  );
-  await sharp(new Buffer(file.data))
-    .resize({ width: 360 })
-    .toBuffer()
-    .then((data) => {
-      streamifier.createReadStream(data).pipe(
-        imageBucket
-          .file(`${folder}/small/${fileName}.${fileExtension}`)
-          .createWriteStream({
-            resumable: false,
-            gzip: true,
-          })
-      );
-    })
-    .catch((e) => console.log(e));
-  await sharp(new Buffer(file.data))
-    .resize({ width: 720 })
-    .toBuffer()
-    .then((data) => {
-      streamifier.createReadStream(data).pipe(
-        imageBucket
-          .file(`${folder}/medium/${fileName}.${fileExtension}`)
-          .createWriteStream({
-            resumable: false,
-            gzip: true,
-          })
-      );
-    })
-    .catch((e) => console.log(e));
-  req.images = {
-    small: `https://storage.googleapis.com/fb-clone-images/${folder}/small/${fileName}.${fileExtension}`,
-    medium: `https://storage.googleapis.com/fb-clone-images/${folder}/medium/${fileName}.${fileExtension}`,
-    original: `https://storage.googleapis.com/fb-clone-images/${folder}/original/${fileName}.${fileExtension}`,
-  };
+      .catch((e) => console.log(e));
+    await sharp(new Buffer(file.data))
+      .resize({ width: 720 })
+      .toBuffer()
+      .then((data) => {
+        streamifier.createReadStream(data).pipe(
+          imageBucket
+            .file(`${folder}/medium/${fileName}.${fileExtension}`)
+            .createWriteStream({
+              resumable: false,
+              gzip: true,
+            })
+        );
+      })
+      .catch((e) => console.log(e));
+    req.images = {
+      small: `https://storage.googleapis.com/fb-clone-images/${folder}/small/${fileName}.${fileExtension}`,
+      medium: `https://storage.googleapis.com/fb-clone-images/${folder}/medium/${fileName}.${fileExtension}`,
+      original: `https://storage.googleapis.com/fb-clone-images/${folder}/original/${fileName}.${fileExtension}`,
+    };
+    req.hasImage = true;
+  } else {
+    req.hasImage = false;
+  }
+
   next();
 };
